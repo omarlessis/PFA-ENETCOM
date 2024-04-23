d91942d22d1238b44d6ae743fd21802568a71db3
omarlessis <83579753+omarlessis@users.noreply.github.com>
2024-04-23 20:46:10 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb+srv://root:root@cluster0.bdozs6y.mongodb.net/pfa?retry
   server.listen(9000, () => {
     console.log('Server started on port 9000');
   });
-  console.log('Database connection successful Trigger......chahiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin ');
+  console.log('Database connection''''Lastest Version'''''''''');
 })
 .catch((error) => {
   console.log('Version 2 Database connection error: ' + error);
